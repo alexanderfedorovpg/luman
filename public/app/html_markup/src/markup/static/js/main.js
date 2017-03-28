da87a6f11f69c17e951d0eb04affba4c8fbeada4
Vitaliy Petrov <v.petrov@digitalwand.ru>
2017-03-29 00:00:07 +0300

    feed page & ui elements

--- public/app/html_markup/src/markup/static/js/main.js
+++ public/app/html_markup/src/markup/static/js/main.js
@@ -5,4 +5,29 @@ import polyfills from './libraries/polyfills';
 $(() => {
     polyfills.init();
     // ================ Здесь инициализируем модули =====================
+
+    // autoresize textarea on load
+    $('.textarea').each(function () {
+        this.style.height = (this.scrollHeight + 10) + 'px';
+    });
+
+    // show/hide select dropdown with photo
+    $('.select').each(function () {
+
+
+        let $self = $(this),
+            $input = $self.find('.input');
+
+        $input.on('click', function (e) {
+            e.stopPropagation();
+            $self.find('.select-dropdown').fadeIn(400);
+        });
+
+    });
+
+    $(document).on('click', function () {
+        $('.select-dropdown').fadeOut(400);
+    });
+
+    // $('textarea').height( $('textarea')[0].scrollHeight );
 });
