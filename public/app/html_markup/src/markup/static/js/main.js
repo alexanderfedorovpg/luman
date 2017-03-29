a6f848ee2356bd2b150408e82bb8d376f3ddfa98
Vitaliy Petrov <v.petrov@digitalwand.ru>
2017-03-29 16:21:27 +0300

    side nav animamion

--- public/app/html_markup/src/markup/static/js/main.js
+++ public/app/html_markup/src/markup/static/js/main.js
@@ -27,6 +27,23 @@ $(() => {
 
     $(document).on('click', function () {
         $('.select-dropdown').fadeOut(400);
+        // $('body').removeClass('is-open-nav');
+        $('.nav-side').removeClass('is-active');
+        $('.content').removeClass('is-moved-content');
+        $('.header').removeClass('is-moved-header');
+    });
+
+    // side nav toggle
+    $('.header-info__hamburger').on('click', function (e) {
+        e.stopPropagation();
+        // $('body').toggleClass('is-open-nav');
+        $('.nav-side').toggleClass('is-active');
+        $('.content').toggleClass('is-moved-content');
+        $('.header').toggleClass('is-moved-header');
+    });
+
+    $('.nav-side').on('click', function (e) {
+        e.stopPropagation();
     });
 
     // $('textarea').height( $('textarea')[0].scrollHeight );
