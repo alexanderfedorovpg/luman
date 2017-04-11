d86fc937dbea9a319797e0499b5ef57177931866
Anna <mattomanka@gmail.com>
2017-04-11 03:08:20 +0300

    Profile fix, add profile history

--- public/app/html_markup/src/markup/components/profile-history/data/data.js
+++ public/app/html_markup/src/markup/components/profile-history/data/data.js
@@ -1,1 +1,94 @@
-var data = {profile-history: {}}
+var data = {history: {
+    titles: [
+        { name: 'Дата', class: 'date' },
+        { name: 'Время', class: 'time' },
+        { name: 'Событие', type: 'sort', class: 'event' },
+        { name: 'Сессия', class: 'session' },
+        { name: 'Хост', class: 'host' },
+        { name: 'IP', class: 'ip' },
+        { name: 'Просмотр', class: 'link' }
+    ],
+    values: [
+        {
+            date: '14.01.17',
+            time: '09:12:10',
+            event: 'login',
+            session: '94.230.115.33',
+            host: '',
+            ip: '94.230.115.33',
+            link: 'javascript:void(0);'
+        },
+        {
+            date: '14.01.17',
+            time: '09:09:10',
+            event: 'Создание страницы',
+            session: '94.230.115.33',
+            host: '',
+            ip: '94.230.115.33',
+            link: 'javascript:void(0);'
+        },
+        {
+            date: '14.01.17',
+            time: '09:01:10',
+            event: 'logout',
+            session: '94.230.115.33',
+            host: '',
+            ip: '94.230.115.33',
+            link: 'javascript:void(0);'
+        },
+        {
+            date: '14.01.17',
+            time: '08:54:10',
+            event: 'Создание страницы',
+            session: '94.230.115.33',
+            host: '',
+            ip: '94.230.115.33',
+            link: 'javascript:void(0);'
+        },
+        {
+            date: '14.01.17',
+            time: '08:46:10',
+            event: 'login error',
+            session: '94.230.115.33',
+            host: '',
+            ip: '94.230.115.33',
+            link: 'javascript:void(0);'
+        },
+        {
+            date: '14.01.17',
+            time: '08:25:10',
+            event: 'Удаление страницы',
+            session: '94.230.115.33',
+            host: '',
+            ip: '94.230.115.33',
+            link: 'javascript:void(0);'
+        },
+        {
+            date: '14.01.17',
+            time: '09:01:10',
+            event: 'Авторизация',
+            session: '94.230.115.33',
+            host: '',
+            ip: '94.230.115.33',
+            link: 'javascript:void(0);'
+        },
+        {
+            date: '14.01.17',
+            time: '08:54:10',
+            event: 'Создание страницы',
+            session: '94.230.115.33',
+            host: '',
+            ip: '94.230.115.33',
+            link: 'javascript:void(0);'
+        },
+        {
+            date: '14.01.17',
+            time: '08:46:10',
+            event: 'Сохранение страницы',
+            session: '94.230.115.33',
+            host: '',
+            ip: '94.230.115.33',
+            link: 'javascript:void(0);'
+        }
+    ]
+}};
