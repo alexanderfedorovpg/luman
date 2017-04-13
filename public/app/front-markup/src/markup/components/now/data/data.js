b39f52d337e59f5147a284ccac8895b16c84a604
nikalun <emptyfull@rambler.ru>
2017-04-13 17:56:59 +0300

    index page: header; news: basic news, now news

---
+++ public/app/front-markup/src/markup/components/now/data/data.js
@@ -0,0 +1,26 @@
+var data = {now: {
+    1: {
+        img: '%=static=%img/content/cow/peskov.jpg',
+        title: 'Овечкина и Малкина не пустили на Олимпиаду',
+        when: 'Сегодня, 15:02',
+        alt: ''
+    },
+    2: {
+        img: '%=static=%img/content/cow/peskov.jpg',
+        title: 'Овечкина и Малкина не пустили на Олимпиаду',
+        when: 'Сегодня, 15:02',
+        alt: ''
+    },
+    3: {
+        img: '%=static=%img/content/cow/peskov.jpg',
+        title: 'МВФ одобрили выеделение $1 млрд для Украины',
+        when: 'Сегодня, 15:02',
+        alt: ''
+    },
+    4: {
+        img: '%=static=%img/content/cow/peskov.jpg',
+        title: 'Новая газета рассказала о секретных тюрьмах для геев в Чечне',
+        when: 'Сегодня, 15:02',
+        alt: ''
+    }
+}};
