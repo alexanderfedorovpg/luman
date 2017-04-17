af036d8d583eb852b5d3f17caf0a45d406190b82
nikalun <emptyfull@rambler.ru>
2017-04-17 18:06:28 +0300

    Index page full, burger-menu, about-company (80%)

---
+++ public/app/front-markup/src/markup/components/subscribe-networks/data/data.js
@@ -0,0 +1,23 @@
+var data = {subscribe_networks: {
+    footer: {
+        title: 'Подпишитесь на нас',
+        ico: {
+            'facebook': '%=static=%img/content/subscribe-networks/facebook.png',
+            'twitter': '%=static=%img/content/subscribe-networks/twitter.png',
+            'youtube': '%=static=%img/content/subscribe-networks/youtube.png',
+            'viber': '%=static=%img/content/subscribe-networks/viber.png',
+            'ok': '%=static=%img/content/subscribe-networks/ok.png',
+            'rss': '%=static=%img/content/subscribe-networks/rss.png',
+        }
+    },
+    menu: {
+        ico: {
+            'facebook': '%=static=%img/content/subscribe-networks/facebook.png',
+            'twitter': '%=static=%img/content/subscribe-networks/twitter.png',
+            'youtube': '%=static=%img/content/subscribe-networks/youtube.png',
+            'viber': '%=static=%img/content/subscribe-networks/viber.png',
+            'ok': '%=static=%img/content/subscribe-networks/ok.png',
+            'rss': '%=static=%img/content/subscribe-networks/rss.png',
+        }
+    }
+}};
