ba369a90b5e87e45068d4b037165f48c73b17a0d
Kosyanenko Aleksey <lehakos@gmail.com>
2017-04-20 17:52:31 +0400

    load programs from api

--- public/app/react_project/app/containers/ProgramsPage/constants.js
+++ public/app/react_project/app/containers/ProgramsPage/constants.js
@@ -5,6 +5,9 @@
  */
 
 export const SET_FILTER = 'app/ProgramsPage/SET_FILTER';
+export const LOAD_PROGRAMS = 'app/ProgramsPage/LOAD_PROGRAMS';
+export const LOAD_PROGRAMS_SUCCESS = 'app/ProgramsPage/LOAD_PROGRAMS_SUCCESS';
+export const LOAD_PROGRAMS_FAILURE = 'app/ProgramsPage/LOAD_PROGRAMS_FAILURE';
 
 export const filters = [
     {
--- public/app/react_project/app/containers/ProgramsPage/sagas.js
+++ public/app/react_project/app/containers/ProgramsPage/sagas.js
@@ -1,11 +1,36 @@
-// import { take, call, put, select } from 'redux-saga/effects';
+import { call, put, takeLatest } from 'redux-saga/effects';
+import * as api from 'api';
+import {
+    programsLoaded,
+    loadingProgramsError,
+} from './actions';
+import {
+    LOAD_PROGRAMS,
+} from './constants';
+
+export function* getPrograms() {
+    try {
+        const response = yield call(api.getPrograms);
+        const payload = response.data.map((program) => (
+            {
+                title: program.name,
+                date: program.updated_at ? program.updated_at : program.created_at,
+                id: program.id,
+            }
+        ));
+
+        yield put(programsLoaded(payload));
+    } catch (err) {
+        yield put(loadingProgramsError(err));
+    }
+}
 
 // Individual exports for testing
-export function* defaultSaga() {
-  // See example in containers/HomePage/sagas.js
+export function* programsData() {
+    yield takeLatest(LOAD_PROGRAMS, getPrograms);
 }
 
 // All sagas to be loaded
 export default [
-  defaultSaga,
+    programsData,
 ];
