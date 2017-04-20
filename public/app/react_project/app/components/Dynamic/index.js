d1918678e91f981f4b6fea8415c4b497cad568a9
i.bikmaev@Ilnur Bikmaev <i.bikmaev@digitalwand.ru>
2017-04-20 10:24:03 +0500

    arrows for dynamic

--- public/app/react_project/app/components/Dynamic/index.js
+++ public/app/react_project/app/components/Dynamic/index.js
@@ -41,6 +41,7 @@ const Title = styled.a`
     color: #000000;
     text-decoration: none;
     text-transform: uppercase;
+    cursor: pointer;
 
     span {
         margin-right: 3px;
@@ -62,10 +63,32 @@ const Wrap = styled.div`
 const Chart = styled.div`
     width: 709px;
     height: 270px;
+    position: relative;
 
     canvas {
         height: 270px !important
     }
+`
+
+const Arrow = styled.a`
+    cursor: pointer;
+    font-weight: 100;
+    position: absolute;
+    bottom: 1px;
+    transition: font-weight .3s ease;
+
+    &.left {
+        left: -9px;
+    }
+
+    &.right {
+        right: -9px;
+    }
+
+    &:hover {
+        font-weight: bold;
+    }
+
 `
 
 
@@ -73,11 +96,31 @@ class Dynamic extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            modalOpen: false
+            modalOpen: false,
+            chartData: {
+                labels: ['06.01.2017', '07.01.2017', '08.01.2017', '09.01.2017', '10.01.2017', '11.01.2017', '12.01.2017', '13.01.2017', '14.01.2017', '15.01.2017'],
+                datasets: [{
+                    data: [12, 19, 3, 17, 6, 3, 7, 4, 5, 6],
+                    fill: false,
+                    lineTension: 0,
+                    pointBackgroundColor: '#359918',
+                    pointRadius: 4,
+                    pointHoverRadius: 4,
+                    borderColor: '#359918',
+                    borderWidth: 1
+                }]
+            },
+            showOld: true,
+            showNew: false
         };
 
         this.openModal = this.openModal.bind(this);
         this.closeModal = this.closeModal.bind(this);
+        this.renderNew = this.renderNew.bind(this);
+        this.renderOld = this.renderOld.bind(this);
+        // this.chartData = {
+
+        // };
     }
 
     openModal() {
@@ -96,22 +139,51 @@ class Dynamic extends Component {
         }
     }
 
+    renderNew() {
+        this.setState({
+            chartData: {
+                labels: ['26.12.2017', '27.12.2017', '28.12.2017', '29.12.2017', '30.12.2016', '31.12.2016', '01.01.2017', '02.01.2017', '04.01.2017', '05.01.2017'],
+                datasets: [{
+                    data: [1, 1, 1, 17, 9, 8, 6, 5, 3, 1],
+                    fill: false,
+                    lineTension: 0,
+                    pointBackgroundColor: '#359918',
+                    pointRadius: 4,
+                    pointHoverRadius: 4,
+                    borderColor: '#359918',
+                    borderWidth: 1
+                }]
+            },
+            showOld: false,
+            showNew: true
+        })
+        //  this.chartData.labels = this.newLabels;
+        //  this.chartData.datasets[0].data = this.newData;
+    }
+
+    renderOld() {
+        this.setState({
+            chartData: {
+                labels: ['06.01.2017', '07.01.2017', '08.01.2017', '09.01.2017', '10.01.2017', '11.01.2017', '12.01.2017', '13.01.2017', '14.01.2017', '15.01.2017'],
+                datasets: [{
+                    data: [12, 19, 3, 17, 6, 3, 7, 4, 5, 6],
+                    fill: false,
+                    lineTension: 0,
+                    pointBackgroundColor: '#359918',
+                    pointRadius: 4,
+                    pointHoverRadius: 4,
+                    borderColor: '#359918',
+                    borderWidth: 1
+                }]
+            },
+            showOld: true,
+            showNew: false
+        })
+    }
+
     render() {
         let rubrics = ['Политика', 'Экономика', 'Общество', 'Наука', 'Спорт', 'Культура', 'Бизнес'];
 
-        let chartData = {
-            labels: ['06.01.2017', '07.01.2017', '08.01.2017', '09.01.2017', '10.01.2017', '11.01.2017', '12.01.2017', '13.01.2017', '14.01.2017', '15.01.2017'],
-            datasets: [{
-                data: [12, 19, 3, 17, 6, 3, 7, 4, 5, 6],
-                fill: false,
-                lineTension: 0,
-                pointBackgroundColor: '#359918',
-                pointRadius: 4,
-                pointHoverRadius: 4,
-                borderColor: '#359918',
-                borderWidth: 1
-            }]
-        };
 
         let options = {
             scaleShowLabels: false,
@@ -158,8 +230,9 @@ class Dynamic extends Component {
                                 value={vall}
                                 onChange={changeEvent} />
                             <Chart>
-                                <Line data={chartData} options={options} redraw={true} />
-                                {/*<RC2 data={chartData} options={options} type='line' />*/}
+                                <Line data={this.state.chartData} options={options} redraw={true} />
+                                {this.state.showOld ? <Arrow className="left" onClick={this.renderNew}>{'<'}</Arrow> : null}
+                                {this.state.showNew ? <Arrow className="right" onClick={this.renderOld}>{'>'}</Arrow> : null}
                             </Chart>
                         </Wrap>
                     </Root >
