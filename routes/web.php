3a48204aab4eb5bfcc14a3946cb8dd3013495807
Михаил Шакин <shakinm@gmail.com>
2017-04-25 16:57:30 +0700

    roolback

--- routes/web.php
+++ routes/web.php
@@ -40,7 +40,7 @@ $app->group(['prefix' => 'api/v1', 'namespace'=>'\App\Http\Controllers\v1'], fun
     $group->get('/newseditor/{id}','NewsListEditorController@getOne');
     $group->post('/newseditor','NewsListEditorController@create');
 
-    $group->put('/newseditor/edit/{id}','NewsListEditorController@edit');
+    $group->put('/newseditor/edit','NewsListEditorController@edit');
     $group->put('/newseditor/publish/{id}','NewsListEditorController@publish');
 
     $group->delete('/newseditor/{id}','NewsListEditorController@delete');
