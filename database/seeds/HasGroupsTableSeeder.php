76a4e9779519c59988ffd21089667dbc53aa4cd5
Михаил Шакин <shakinm@gmail.com>
2017-04-26 12:30:55 +0700

    Preparing the installation manual and the project files for a release

---
+++ database/seeds/HasGroupsTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class HasGroupsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user=App\Models\User::firstOrFail();
+        $group=App\Models\Groups::firstOrFail();
+
+        DB::table('groups')->insert([
+            'user_id' => $user->id,
+            'group_id'=> $group->id,
+        ]);
+
+    }
+}
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'administrator',
+            'email' => 'admin@example.com',
+            'password' => bcrypt('admin'),
+            'need_change_password'=>'0',
+            'enabled'=>'1',
+        ]);
+    }
+}
