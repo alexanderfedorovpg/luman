bb0439df5c4224eb69e3cdec7f98bf530e9b5fd5
Anna <mattomanka@gmail.com>
2017-05-05 07:20:24 +0300

    Subscribe hover

--- public/app/front-markup/src/markup/components/subscribe-networks/data/data.js
+++ public/app/front-markup/src/markup/components/subscribe-networks/data/data.js
@@ -19,5 +19,13 @@ var data = {subscribe_networks: {
             'ok': '%=static=%img/content/subscribe-networks/ok.png',
             'rss': '%=static=%img/content/subscribe-networks/rss.png',
         }
+    },
+    subscribe: {
+        ico: {
+            'facebook': '%=static=%img/content/subscribe-networks/fb.png',
+            'twitter': '%=static=%img/content/subscribe-networks/tw.png',
+            'ok': '%=static=%img/content/subscribe-networks/okk.png',
+            'vk': '%=static=%img/content/subscribe-networks/vkk.png',
+        }
     }
 }};
