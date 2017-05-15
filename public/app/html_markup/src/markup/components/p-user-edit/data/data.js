25231d1dab3227679313209d93d23916b2b36d29
Vitaliy Petrov <v.petrov@digitalwand.ru>
2017-05-15 18:00:09 +0300

    fix includes, that slowing build process. user-edit page

---
+++ public/app/html_markup/src/markup/components/p-user-edit/data/data.js
@@ -0,0 +1,42 @@
+var data = {userEdit: {
+    items: [
+        {
+            userName: 'Поликарпов Анатолий',
+            userPic: '%=static=%img/content/user3.png',
+            group: 'Редактор',
+            status: 'Активен'
+        },
+        {
+            userName: 'Ковалев Максим',
+            userPic: '%=static=%img/content/user1.png',
+            group: 'Редактор',
+            status: 'Активен'
+        },
+        {
+            userName: 'Короленко Анастасия',
+            userPic: '%=static=%img/content/user2.png',
+            group: 'Практикант',
+            status: 'Заблокирован',
+            class: 'is-blocked'
+        },
+        {
+            userName: 'Константинов Виталий',
+            userPic: '%=static=%img/content/user.png',
+            group: 'Выпускающий',
+            status: 'Активен'
+        },
+        {
+            userName: 'Марышева Елена',
+            userPic: '%=static=%img/content/user4.png',
+            group: 'Редактор',
+            status: 'Заблокирован',
+            class: 'is-blocked'
+        },
+        {
+            userName: 'Санченков Роман',
+            userPic: '%=static=%img/content/user5.png',
+            group: 'Практикант',
+            status: 'Активен'
+        }
+    ]
+}}
