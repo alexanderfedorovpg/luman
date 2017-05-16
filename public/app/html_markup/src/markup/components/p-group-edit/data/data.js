7676814412db90febfc83bbe4fa7a77ef34e17d2
Vitaliy Petrov <v.petrov@digitalwand.ru>
2017-05-16 14:26:54 +0300

    user-edit, group-edit, journal

---
+++ public/app/html_markup/src/markup/components/p-group-edit/data/data.js
@@ -0,0 +1,36 @@
+var data = {groupEdit: {
+    items: [
+        {
+            userName: 'Администратор',
+            userPic: 'А',
+            users: '1',
+            status: 'Активна'
+        },
+        {
+            userName: 'Выпускающий',
+            userPic: 'В',
+            users: '2',
+            status: 'Активна'
+        },
+        {
+            userName: 'Редактор',
+            userPic: 'Р',
+            users: '5',
+            status: 'Активна'
+        },
+        {
+            userName: 'Практикант',
+            userPic: 'П',
+            users: '10',
+            status: 'Заблокирована',
+            class: 'is-blocked'
+        },
+        {
+            userName: 'Читатель',
+            userPic: 'Ч',
+            users: '12',
+            status: 'Активна'
+        }
+    ]
+}}
+
--- public/app/html_markup/src/markup/static/js/main.js
+++ public/app/html_markup/src/markup/static/js/main.js
@@ -46,4 +46,30 @@ $(() => {
         e.stopPropagation();
     });
 
+    // для визуального показа переключение между добавлением и редактированием пользователя
+    $('.user-edit-form').find('.icon-delete').on('click', function () {
+        $(this).closest('.user-edit-form').hide();
+        $('.user-edit-form__add').show();
+        $('.user-edit__table').find('tbody tr').removeClass('is-active');
+    });
+
+    $('.user-edit__table').find('tbody tr').on('click', function () {
+        $(this).addClass('is-active').siblings().removeClass('is-active');
+        $('.user-edit-form__add').hide();
+        $('.user-edit-form__add').next().show();
+    });
+
+    $('.group-edit-form').find('.icon-delete').on('click', function () {
+        $(this).closest('.group-edit-form').hide();
+        $('.group-edit-form__add').show();
+        $('.group-edit__table').find('tbody tr').removeClass('is-active');
+    });
+
+    $('.group-edit__table').find('tbody tr').on('click', function () {
+        $(this).addClass('is-active').siblings().removeClass('is-active');
+        $('.group-edit-form__add').hide();
+        $('.group-edit-form__add').next().show();
+    });
+
+
 });
