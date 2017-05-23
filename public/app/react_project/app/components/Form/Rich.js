e46e4aec3af0a8940967b43ad36d018e3aeaaae7
Andrey Minaev <andreyminaev13@gmail.com>
2017-05-23 14:41:58 +0700

    build fix

--- public/app/react_project/app/components/Form/Rich.js
+++ public/app/react_project/app/components/Form/Rich.js
@@ -1,8 +1,8 @@
 import React, { PureComponent } from 'react'
 import styled from 'styled-components'
 import ReactQuill, { Quill } from 'react-quill'
-import Emitter from 'quill/core/emitter'
-import Delta from 'quill-delta'
+// import Emitter from 'quill/core/emitter'
+// import Delta from 'quill-delta'
 
 import 'quill/dist/quill.snow.css'
 
@@ -29,33 +29,33 @@ const modules = {
             ]
         ],
         handlers: {
-            image: function (value) {
-                let fileInput = this.container.querySelector('input.ql-image[type=file]');
-                if (fileInput == null) {
-                    fileInput = document.createElement('input');
-                    fileInput.setAttribute('type', 'file');
-                    fileInput.setAttribute('accept', 'image/png, image/gif, image/jpeg, image/bmp, image/x-icon, image/svg+xml');
-                    fileInput.classList.add('ql-image');
-                    fileInput.addEventListener('change', () => {
-                      if (fileInput.files != null && fileInput.files[0] != null) {
-                        imageHandler(fileInput.files[0], (result) => {
-                        // let reader = new FileReader();
-                        // reader.onload = (e) => {
-                          let range = this.quill.getSelection(true);
-                          this.quill.updateContents(new Delta()
-                            .retain(range.index)
-                            .delete(range.length)
-                            .insert({ image: result })
-                          , Emitter.sources.USER);
-                          fileInput.value = '';
-                        });
-                        // reader.readAsDataURL(fileInput.files[0]);
-                      }
-                    });
-                    this.container.appendChild(fileInput);
-                }
-                fileInput.click();
-            }
+            // image: function (value) {
+            //     let fileInput = this.container.querySelector('input.ql-image[type=file]');
+            //     if (fileInput == null) {
+            //         fileInput = document.createElement('input');
+            //         fileInput.setAttribute('type', 'file');
+            //         fileInput.setAttribute('accept', 'image/png, image/gif, image/jpeg, image/bmp, image/x-icon, image/svg+xml');
+            //         fileInput.classList.add('ql-image');
+            //         fileInput.addEventListener('change', () => {
+            //           if (fileInput.files != null && fileInput.files[0] != null) {
+            //             imageHandler(fileInput.files[0], (result) => {
+            //             // let reader = new FileReader();
+            //             // reader.onload = (e) => {
+            //               let range = this.quill.getSelection(true);
+            //               this.quill.updateContents(new Delta()
+            //                 .retain(range.index)
+            //                 .delete(range.length)
+            //                 .insert({ image: result })
+            //               , Emitter.sources.USER);
+            //               fileInput.value = '';
+            //             });
+            //             // reader.readAsDataURL(fileInput.files[0]);
+            //           }
+            //         });
+            //         this.container.appendChild(fileInput);
+            //     }
+            //     fileInput.click();
+            // }
         }
     },
 };
