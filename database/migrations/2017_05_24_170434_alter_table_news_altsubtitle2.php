6f9d0b4ed273509942cf1e203bbb40decf9fc65d
Михаил Шакин <shakinm@gmail.com>
2017-05-24 16:45:33 +0700

    fix

--- database/migrations/2017_05_24_170434_alter_table_news_altsubtitle2.php
+++ database/migrations/2017_05_24_170434_alter_table_news_altsubtitle2.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AlterTableNewsAltsubtitle extends Migration
+class AlterTableNewsAltsubtitle2 extends Migration
 {
     /**
      * Run the migrations.
