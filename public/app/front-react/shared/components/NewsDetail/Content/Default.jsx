6bcd97b026e53e52becb2dae4904ea96ace68fe9
Сергей Самохвалов <s.samohvalov@digitalwand.ru>
2017-05-25 18:11:50 +0800

    соц. кнопки

--- public/app/front-react/shared/components/NewsDetail/Content/Default.jsx
+++ public/app/front-react/shared/components/NewsDetail/Content/Default.jsx
@@ -53,7 +53,7 @@ function Content({ data, children }) {
                         {data.sub_title}
                     </p>
                 </div>
-                <Socials />
+                <Socials shareLink={data.share_link} title={data.title}/>
             </div>
             <div className="inner-about__content">
                 <div dangerouslySetInnerHTML={{ __html: data.body }} />
