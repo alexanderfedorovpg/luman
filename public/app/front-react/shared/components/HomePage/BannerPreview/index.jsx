f6baab024e7592c196ab6433a02b4c962395be6a
Николай Лукиных <n.lukinyh@digitalwand.ru>
2017-06-02 16:39:05 +0300

    Адаптив главной, передлка блок с баннерами-превью

--- public/app/front-react/shared/components/HomePage/BannerPreview/index.jsx
+++ public/app/front-react/shared/components/HomePage/BannerPreview/index.jsx
@@ -7,49 +7,11 @@ function BannerPreview({ className }) {
 
     return (
         <div className={classNames('banner-preview', className)}>
-            <img className="banner-preview__img" src="/content/banner/banner.png" alt="" role="presentation" />
-            <div className="banner-preview__left-part">
-                <p className="banner-preview__title-group">
-                    RTVI
-                </p>
-                <p className="banner-preview__subtitle-group">
-                    Наши эфиры
-                </p>
-            </div>
-            <div className="banner-preview__right-part">
-                <div className="banner-preview__wrapper">
-                    <div className="banner-preview__part-title">
-                        <p className="banner-preview__title-programm">
-                            Тайм код: итоги недели
-                        </p>
-                        <p className="banner-preview__subtitle-programm">
-                            С Владимиром Ленским
-                        </p>
-                    </div>
-                    <div className="banner-preview__delimeter banner-preview__delimeter banner-preview__delimeter_one">
-                        <img src="/content/banner/delimeter.png" alt="" role="presentation" />
-                    </div>
-                    <div className="banner-preview__part-time banner-preview__part-time banner-preview__part-time_one">
-                        <p className="banner-preview__programm-title">
-                            По субботам
-                        </p>
-                        <p className="banner-preview__programm-time">
-                            19:00
-                        </p>
-                    </div>
-                    <div className="banner-preview__delimeter banner-preview__delimeter banner-preview__delimeter_two">
-                        <img src="/content/banner/delimeter.png" alt="" role="presentation" />
-                    </div>
-                    <div className="banner-preview__part-time">
-                        <p className="banner-preview__programm-title">
-                            По воскресеньям
-                        </p>
-                        <p className="banner-preview__programm-time">
-                            11:00
-                        </p>
-                    </div>
-                </div>
-            </div>
+            <picture>
+                <source srcSet="/content/banner/banner-tablet-landscape.png" media="(max-width: 1559px)"/>
+                <source srcSet="/content/banner/banner.png" media="(min-width: 1600px)"/>
+                <img src="/content/banner/banner.png" alt="" role="presentation"/>
+            </picture>
         </div>
     )
 }
