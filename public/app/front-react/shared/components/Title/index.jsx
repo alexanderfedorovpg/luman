f11e633675269b9f5f513f9b2d19c52caf9cfab4
i.bikmaev@Ilnur Bikmaev <i.bikmaev@digitalwand.ru>
2017-06-08 17:00:17 +0500

    infoblock page

--- public/app/front-react/shared/components/Title/index.jsx
+++ public/app/front-react/shared/components/Title/index.jsx
@@ -1,20 +1,30 @@
 import React from 'react'
 import classNames from 'classnames'
+import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types';
 
 import IconInfo from 'components/Icon/Info'
 
 import './style.scss'
 
-function Title({ children, className }) {
+function Title({ children, className, anchor }) {
+
+    let link = anchor ? `/info-page#${anchor}` : '/info-page';
 
     return (
         <div className={classNames('title-block', className)}>
             <h1 className="title-block__title">
                 {children}
-                <IconInfo className="title-block__ico title-block__ico_info" />
+                <Link to={link}>
+                    <IconInfo className="title-block__ico title-block__ico_info" />
+                </Link>
             </h1>
         </div>
     )
 }
+
+Title.propTypes = {
+  anchor: PropTypes.string,
+};
 
 export default Title
