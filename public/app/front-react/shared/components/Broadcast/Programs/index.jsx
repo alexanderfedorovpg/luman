a3484a4ab5a496697a2fed56d546dd1cb47d851d
Николай Лукиных <n.lukinyh@digitalwand.ru>
2017-06-15 15:11:52 +0300

    Страница программы адаптив

--- public/app/front-react/shared/components/Broadcast/Programs/index.jsx
+++ public/app/front-react/shared/components/Broadcast/Programs/index.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import MediaQuery from 'react-responsive'
 
 import Title from 'components/Title'
 import Tabs from 'components/Tabs'
@@ -17,7 +18,7 @@ import MoreNews from 'components/HomePage/MoreNews'
 import LoadMore from 'components/LoadMore'
 import Video from 'components/Aside/Video'
 
-import Datepicker from 'components/Datepicker' 
+import Datepicker from 'components/Datepicker'
 
 import './style.scss'
 
@@ -64,8 +65,8 @@ class Broadcast extends Component {
 
         return (
             <div className="inner-wrapper">
-                <div className="news-top">
-                    <div className="news-top__container container">
+                <div className="news-top news-top_programs">
+                    <div className="news-top__container news-top__container-programs container">
                         <div className="news-top__left news-top__full">
                             <Title>
                                 Из эфира
@@ -73,7 +74,7 @@ class Broadcast extends Component {
                             <Tabs data={programs} active={program} onChange={setProgram} />
                         </div>
                     </div>
-                    <div className="news-header">
+                    <div className="news-header news-top__news-header">
                         <div className="news-header__title">
                             <div className="container news-header__container">
                                 {programs.filter(v => v.id === program)[0].name}
@@ -82,7 +83,7 @@ class Broadcast extends Component {
                             </div>
                         </div>
                         <div className="news-header__content">
-                            <div className="container">
+                            <div className="container news-header__container news-header__container_personal-description">
                                 <div className="news-header__content-container">
                                     <p>
                                         Лиза Каймин - наш проводник в мир кичта и гламура американской тусовки.
@@ -98,55 +99,111 @@ class Broadcast extends Component {
                                     </p>
                                 </div>
                             </div>
-                            <div className="container">
+                            <div className="container news-header__container  news-header__container_tabs">
                                 <div className="news-header__tabs">
                                     <div className="news-header__tabs_item active">Лучшие моменты</div>
                                     <div className="news-header__tabs_item">Все выпуски</div>
                                 </div>
                             </div>
+                            <div className="container news-header__container news-header__container_personality"></div>
                         </div>
                     </div>
-                    <div className="news-top__container container no-flex">
-                        <div className="news-top__left">
-                            <div className="news-one-line news-top__news-one-line">
-                                <div className="news-one-line__row news-one-line__item-bottom" style={{ marginBottom: 30 }}>
-                                    <Item data={data[0]} big />
-                                    <div className="news-one-line__date-wrap">
-                                        <div className="news-one-line__date">
-                                            <div>Поиск выпуска по дате эфира: c <Datepicker className='small' /> по <Datepicker className='small' /> <a href="#" className='button' role="button">Показать</a></div>
-                                        </div>
-                                        <div className="news-one-line__date-items">
-                                            <Item data={data[5]} />
-                                            <Item data={data[6]} />
+                    <div className="news-top__container container">
+                        <div className="middle-col">
+                            <div className="news-top__left">
+                                <div className="news-one-line news-top__news-one-line">
+                                    <div className="news-one-line__row news-one-line__item-bottom">
+                                        <MediaQuery maxWidth="1249px">
+                                            <div className="news-one-line__date-wrap">
+                                                <div className="news-one-line__date">
+                                                    <div>
+                                                        <span className="news-one-line__date-title">Поиск выпуска по дате эфира:</span>
+                                                        <span className="news-one-line__from-text"> c </span>
+                                                        <Datepicker className='small datepicker_programm' />
+                                                        <span className="news-one-line__to-text">по</span>
+                                                        <Datepicker className='small datepicker_programm' />
+                                                        <a href="#" className='button' role="button">Показать</a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </MediaQuery>
+                                        <Item data={data[0]} big />
+                                        <div className="news-one-line__date-wrap news-one-line__date-wrap_view-desktop">
+                                            <MediaQuery minWidth="1250px">
+                                                <div className="news-one-line__date">
+                                                    <div>
+                                                        <span className="news-one-line__date-title">Поиск выпуска по дате эфира:</span>
+                                                        <span className="news-one-line__from-text"> c </span>
+                                                        <Datepicker className='small datepicker_programm' />
+                                                        <span className="news-one-line__to-text">по</span>
+                                                        <Datepicker className='small datepicker_programm' />
+                                                        <a href="#" className='button' role="button">Показать</a>
+                                                    </div>
+                                                </div>
+                                            </MediaQuery>
+                                            <div className="news-one-line__date-items">
+                                                <Item data={data[5]} />
+                                                <Item data={data[6]} />
+                                            </div>
                                         </div>
                                     </div>
+                                    {/*<div className="news-one-line__row">
+                                     <Item data={data[7]} />
+                                     </div>*/}
                                 </div>
-                                {/*<div className="news-one-line__row">
-                                    <Item data={data[7]} />
-                                </div>*/}
                             </div>
                         </div>
-                        <div className="right-col">
-                            {/*<Video data={{}} /> */}
+                        <MediaQuery maxWidth="1249px">
+                            <div className="news-top__middle middle-col">
+                                <BannerPreview className="news-top__banner-preview" />
+                            </div>
+                        </MediaQuery>
+
+                        <MediaQuery minWidth="1250px">
+                            <div className="left-col left-col_width_inner">
+                                <Banner className="news-top__banner" />
+                            </div>
+                        </MediaQuery>
+                        <MediaQuery minWidth="1250px">
+                            <div className="right-col">
+                                <Subscribe className="news-top__subscribe-programs" />
+                            </div>
+                        </MediaQuery>
+
+
+                        <div className="middle-col">
+                            <div className="news-one-line__row">
+                                <Item data={data[8]} />
+                                <Item data={data[9]} />
+                                <Item data={data[10]} />
+                                <Item data={data[11]} />
+                            </div>
                         </div>
-                        <div className="news-one-line__row">
-                            <Banner className="news-top__banner" />
-                            <Subscribe />
-                        </div>
-                        <div className="news-one-line__row">
-                            <Item data={data[8]} />
-                            <Item data={data[9]} />
-                            <Item data={data[10]} />
-                            <Item data={data[11]} />
-                        </div>
-                        <div className="news-top__middle middle-col">
-                            <BannerPreview className="news-top__banner-preview" />
-                        </div>
-                        <div className="news-one-line__row">
-                            <Item data={data[12]} />
-                            <Item data={data[13]} />
-                            <Item data={data[14]} />
-                            <Item data={data[15]} />
+
+                        <MediaQuery minWidth="1250px">
+                            <div className="news-top__middle middle-col">
+                                <BannerPreview className="news-top__banner-preview" />
+                            </div>
+                        </MediaQuery>
+
+                        <MediaQuery maxWidth="1249px">
+                            <div className="left-col left-col_width_inner">
+                                <Banner className="news-top__banner" />
+                            </div>
+                        </MediaQuery>
+                        <MediaQuery maxWidth="1249px">
+                            <div className="right-col">
+                                <Subscribe className="news-top__subscribe-programs" />
+                            </div>
+                        </MediaQuery>
+
+                        <div className="middle-col">
+                            <div className="news-one-line__row">
+                                <Item data={data[12]} />
+                                <Item data={data[13]} />
+                                <Item data={data[14]} />
+                                <Item data={data[15]} />
+                            </div>
                         </div>
                         {this.renderAdditionalData(data.slice(16))}
                         {canLoad
