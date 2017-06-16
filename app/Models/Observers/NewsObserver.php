f4ec624b7bacb6571ae9751348d672f660f7543a
Михаил Шакин <shakinm@gmail.com>
2017-06-17 00:51:53 +0700

    link in ti rhe stream url

--- app/Models/Observers/NewsObserver.php
+++ app/Models/Observers/NewsObserver.php
@@ -15,13 +15,13 @@ class NewsObserver
     public function saved(News $news)
     {
 
-        if ($news['attributes']['is_publish'] == 0 && $news['original']['is_publish'] == 1) {
-
-            HomepageNews::destroy($news['attributes']['id']);
-            HomepageInfoNoise::destroy($news['attributes']['id']);
-            HomepageWar::destroy($news['attributes']['id']);
-            HomepageRecord::destroy($news['attributes']['id']);
-        }
+//        if ($news['attributes']['is_publish'] == 0 && $news['original']['is_publish'] == 1) {
+//
+//            HomepageNews::destroy($news['attributes']['id']);
+//            HomepageInfoNoise::destroy($news['attributes']['id']);
+//            HomepageWar::destroy($news['attributes']['id']);
+//            HomepageRecord::destroy($news['attributes']['id']);
+//        }
         if (env('CACHE_DRIVER') == 'redis') {
             if ($news['attributes']['is_publish'] == 1) {
                 Cache::flush();
