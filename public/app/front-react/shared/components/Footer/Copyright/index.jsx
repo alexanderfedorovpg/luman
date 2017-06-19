9f5554ba9aa87029fb1ab85a361b313b77a9eaf8
ivanov <1>
2017-06-19 04:58:46 +0300

    footer

--- public/app/front-react/shared/components/Footer/Copyright/index.jsx
+++ public/app/front-react/shared/components/Footer/Copyright/index.jsx
@@ -6,7 +6,8 @@ function Copyright() {
 
     return (
         <div className="copyright footer__copyright">
-            <p className="copyright__text">&#169; ООО "ЭрТиВиАй Продакшн",
+            <p className="copyright__text">&#169; ООО "ЭрТиВиАй Продакшн", RTVi.RU: Свидетельство о регистрации ЭЛ № ФС 77 - 69385 выдано Роскомнадзором 25.04.2017 года.
+                <br/>Адрес редакции и учредителя: 123022, г. Москва, ул. Рочдельская, д. 15, стр. 8
                 <br/>2002–2017
             </p>
         </div>
