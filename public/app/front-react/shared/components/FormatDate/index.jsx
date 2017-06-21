ef1d805fb8a47c55192532b4b47da9776d9ae6c6
Kosyanenko Aleksey <lehakos@gmail.com>
2017-06-21 21:51:30 +0400

    SITE - show all date with timezone offset

--- public/app/front-react/shared/components/FormatDate/index.jsx
+++ public/app/front-react/shared/components/FormatDate/index.jsx
@@ -1,23 +1,15 @@
 import React from 'react';
 import { FormattedRelative, FormattedDate, FormattedTime } from 'react-intl';
 
+import { convertToLocaleTime } from 'shared/utils/date';
+
 import './style.scss';
 
 const twoDays = 172800000;
 
 function FormatDate({ value }) {
     const now = new Date();
-    let date = null;
-
-    if (value instanceof Date) {
-        date = value;
-    } else if (typeof value === 'string') {
-        // for stupid Safari
-        const fixedVal = value.replace(/-/g, '/');
-        date = Date.parse(fixedVal) && new Date(fixedVal);
-    } else {
-        return null;
-    }
+    const date = convertToLocaleTime(value);
 
     if (!date) {
         return null;
@@ -26,8 +18,17 @@ function FormatDate({ value }) {
     return now.getTime() - date.getTime() >= twoDays
         ? (
             <span className="format-date">
-                <FormattedDate value={date} year="numeric" month="2-digit" day="2-digit" />
-                <FormattedTime value={date} hour="numeric" minute="numeric" />
+                <FormattedDate
+                    value={date}
+                    year="numeric"
+                    month="long"
+                    day="2-digit"
+                />
+                <FormattedTime
+                    value={date}
+                    hour="numeric"
+                    minute="numeric"
+                />
             </span>
         )
         : (
