b7f74159fa6fa15eaf8c1a321489c7ace0c05c81
Andrey Minaev <andreyminaev13@gmail.com>
2017-06-22 17:38:54 +0700

    fix

--- public/app/react_project/app/components/Constructor/Collapse/Article.js
+++ public/app/react_project/app/components/Constructor/Collapse/Article.js
@@ -165,7 +165,7 @@ class Article extends Component {
                                 )
                                 : null
                             }
-                            {true ?
+                            {data.video ?
                                 <Title>
                                     {data.title}
                                 </Title>
