c8731eb145d68ea6654a737d09893b7227cf0b2e
ivanov@loomoon.ru <!Qaz2wsx>
2017-06-24 17:41:27 +0300

    rate

--- public/app/front-react/shared/components/Header/Rates/index.jsx
+++ public/app/front-react/shared/components/Header/Rates/index.jsx
@@ -8,19 +8,19 @@ function Rates() {
         <ul className="rates">
             <li className="rates__item">
                 <span className="rates__name">Доллар</span>
-                <span className="rates__value">60,00</span>
+                <span className="rates__value">59,66</span>
             </li>
             <li className="rates__item">
                 <span className="rates__name">Евро</span>
-                <span className="rates__value">66,80</span>
+                <span className="rates__value">66,68</span>
             </li>
             <li className="rates__item">
                 <span className="rates__name">Шекель</span>
-                <span className="rates__value">16,92</span>
+                <span className="rates__value">16,85</span>
             </li>
             <li className="rates__item">
                 <span className="rates__name">Нефть</span>
-                <span className="rates__value">44,55</span>
+                <span className="rates__value">45,63</span>
             </li>
         </ul>
     )
